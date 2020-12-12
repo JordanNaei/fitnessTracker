6e4a34f316b05e24d6af5a6f8d14affa3527b11b
Jordan Naei <jordannaei.engineer@gmail.com>
2020-12-11 19:32:34 -0500

    doing some testing to analyze atlas Mongo db

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_UR || "mongodb://localhost/workout", {
   useNewUrlParser: true, useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false,
